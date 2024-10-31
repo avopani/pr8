d3e2fd16052075da35cb771a7a9aaae7a197a2f1
Nastya Sharipova <stacy00.ru@gmail.com>
2024-10-31 19:18:42 +0500

    Update zvezda.py

--- homework3/zvezda.py
+++ homework3/zvezda.py
@@ -1,5 +1,10 @@
 
 for i in range(10):
-        print('**********')
+    for j in range(10):
+        if i == 0 or i == 10 - 1 or j == 0 or j == 10 - 1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')  
+    print()
     
       
